ceccb481e67b66c8890982fd9fb3e146268722db
Dev Govindji <friendsofdev94@gmail.com>
2021-02-25 16:05:58 -0600

    completes authentication

--- src/index.js
+++ src/index.js
@@ -3,40 +3,49 @@ const fs = require('fs')
 const path = require('path')
 const { PrismaClient } = require('@prisma/client')
 
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const { APP_SECRET, getUserId } = require('./utils')
+
+
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews clone`,
-        feed: async (parent, args, context, info) => {
-            return context.prisma.link.findMany()
-        },
-    },
-    Mutation: {
-        post: (parent, args, context, info) => {
-            const newLink = context.prisma.link.create({
-                data: {
-                    url: args.url,
-                    description: args.description,
-                },
-            })
-            return newLink
-        },
-        // updateLink: (parent, args) => {
-        //     // find item in Links with id, update to all three fields
-        //     let linkToUpdate = links.find(item => item.id == args.id)
-        //     linkToUpdate.description = args.description
-        //     linkToUpdate.url = args.url
-        // },
-        // deleteLink: (parent, args) => {
-        //     // find id with in Links, delete it
-        //     // let linkToDelete = links.filter(item => item.id == args.id)
-        //     // sieve out id, reassign links to filtered array without id
-        //     links = links.filter(item => item.id != args.id)
-        //     // splice before id, splice after id, join together
+    Query,
+    Mutation,
+    User,
+    Link,
+    // Query: {
+    //     info: () => `This is the API of a Hackernews clone`,
+
+    // },
+    // Mutation: {
+    //     // post: (parent, args, context, info) => {
+    //     //     const newLink = context.prisma.link.create({
+    //     //         data: {
+    //     //             url: args.url,
+    //     //             description: args.description,
+    //     //         },
+    //     //     })
+    //     //     return newLink
+    //     // },
+    //     // updateLink: (parent, args) => {
+    //     //     // find item in Links with id, update to all three fields
+    //     //     let linkToUpdate = links.find(item => item.id == args.id)
+    //     //     linkToUpdate.description = args.description
+    //     //     linkToUpdate.url = args.url
+    //     // },
+    //     // deleteLink: (parent, args) => {
+    //     //     // find id with in Links, delete it
+    //     //     // let linkToDelete = links.filter(item => item.id == args.id)
+    //     //     // sieve out id, reassign links to filtered array without id
+    //     //     links = links.filter(item => item.id != args.id)
+    //     //     // splice before id, splice after id, join together
 
 
-        // },
-    }
+    //     // },
+    // }
 }
 
 const prisma = new PrismaClient()
@@ -44,8 +53,12 @@ const prisma = new PrismaClient()
 const server = new ApolloServer({
     typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
     resolvers,
-    context: {
-        prisma,
+    context: ({ req }) => {
+        return {
+            ...req,
+            prisma,
+            userId: req && req.headers.authorization ? getUserId(req) : null
+        }
     }
 })
 
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
+
 async function signup(parent, args, context, info) {
 
     const password = await bcrypt.hash(args.password, 10)
@@ -21,6 +25,20 @@ async function login(parent, args, context, info) {
 
     return { token, user }
 }
+
+
+async function post(parent, args, context, info) {
+    const { userId } = context
+
+    return await context.prisma.link.create({
+        data: {
+            url: args.url,
+            description: args.description,
+            postedBy: { connect: { id: userId } }
+        }
+    })
+}
+
 
 
 module.exports = {
